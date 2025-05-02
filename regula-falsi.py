227be74a9ea7d59e8aa26f4b2be526f850a53545
kshitijkaithwas <162148869+kshitijkaithwas@users.noreply.github.com>
2025-05-02 10:38:55 +0530

    Create regula-falsi.py

---
+++ regula-falsi.py
@@ -0,0 +1,27 @@
+import math as m
+
+def f(x):
+    return x * m.log(x) - 1.2
+
+def regula_falsi(x0,x1,eps=1e-5):
+    if f(x0) * f(x1) >= 0:
+        print("Regula Falsi method fails.")
+        return None
+    
+    for _ in range(100):
+        x = x0 - (f(x0) * (x1 - x0) / (f(x1) - f(x0)))
+
+        if abs(f(x)) < eps:
+            print(f"Root is: {x}")
+            return x
+        
+        if f(x0) * f(x) < 0:
+            x1 = x
+        else:
+            x0 = x
+
+    return None
+
+x0 = 2
+x1 = 3
+root = regula_falsi(x0,x1)
